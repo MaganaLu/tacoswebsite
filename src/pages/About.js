87fb0f45bbdc414008a2b5253a2c5c51689a3039
lmagana <lmagana@FMHL-4SFPJR3>
2023-10-27 15:34:49 -0700

    .

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,20 +1,109 @@
 import React from 'react';
 import './About.css';
 import pic from '../pictures/skeleton2.jpg';
+import pic2 from '../pictures/ifm-Logo.jpg';
+import Carousel from "react-multi-carousel";
 
 const About = () => {
+    const responsive = {
+        desktop: {
+            breakpoint: {
+                max: 3000,
+                min: 1080
+            },
+            items: 1
+        },
+        mobile: {
+            breakpoint: {
+                max: 464,
+                min: 0
+            },
+            items: 1
+        },
+        tablet: {
+            breakpoint: {
+                max: 1024,
+                min: 464
+            },
+            items: 1
+        }
+    };
+
     return (
         <div class="wrapper">
             <article class="main">
-                <img alt="meme" src={pic} />
-                <img alt="meme" src={pic} />
-                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+                <Carousel
+                    additionalTransfrom={0}
+                    arrows
+                    autoPlaySpeed={3000}
+                    centerMode={false}
+                    className=""
+                    containerClass="container"
+                    dotListClass=""
+                    draggable
+                    focusOnSelect={false}
+                    infinite
+                    itemClass=""
+                    keyBoardControl
+                    minimumTouchDrag={80}
+                    pauseOnHover
+                    renderArrowsWhenDisabled={false}
+                    renderButtonGroupOutside={false}
+                    renderDotsOutside={false}
+                    responsive={responsive}
+                    rewind={false}
+                    rewindWithAnimation={false}
+                    rtl={false}
+                    shouldResetAutoplay
+                    showDots
+                    sliderClass=""
+                    slidesToSlide={1}
+                    swipeable
+                    display= "grid"
+                >
+
+                    <img
+                        src="https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+                        style={{
+                            display: 'block',
+                            height: '100%',
+                            margin: 'auto',
+                            width: '100%'
+                        }}
+                    />
+                    <img
+                        src="https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+                        style={{
+                            display: 'block',
+                            height: '100%',
+                            margin: 'auto',
+                            width: '100%'
+                        }}
+                    />
+                </Carousel>
             </article>
             <aside class="aside aside-1">
-                <img alt="meme" src={pic} />
-                
+                Aside 1
+                <img alt="meme" src={pic2} />
             </aside>
-            <aside class="aside aside-2">Aside 2</aside>
+
+            <aside class="aside aside-1">
+                Aside 1
+                <img alt="meme" src={pic2} />
+            </aside>
+
+            <aside class="aside aside-1">
+                Aside 1
+                <img alt="meme" src={pic2} />
+            </aside>
+
+            
+
+            <div class="break"></div>
+            <aside class="aside aside-3">
+                Aside 3
+                <img alt="meme" src={pic2} />
+            </aside>
             <footer class="footer">Footer</footer>
         </div>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Slideshow from '../components/Slideshow'
 import pic from '../pictures/skeleton2.jpg';
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import pic2 from '../pictures/ifm-Logo.jpg';
 
 const Home = () => {
 
@@ -34,89 +35,79 @@ const Home = () => {
 
     return (
 
-        <div>
-            <Carousel
-                additionalTransfrom={0}
-                arrows
-                autoPlaySpeed={3000}
-                centerMode={false}
-                className=""
-                containerClass="container"
-                dotListClass=""
-                draggable
-                focusOnSelect={false}
-                infinite
-                itemClass=""
-                keyBoardControl
-                minimumTouchDrag={80}
-                pauseOnHover
-                renderArrowsWhenDisabled={false}
-                renderButtonGroupOutside={false}
-                renderDotsOutside={false}
-                responsive={responsive}
-                rewind={false}
-                rewindWithAnimation={false}
-                rtl={false}
-                shouldResetAutoplay
-                showDots
-                sliderClass=""
-                slidesToSlide={1}
-                swipeable
-            >
-                
-                <img
-                    src="https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-                    style={{
-                        display: 'block',
-                        height: '100%',
-                        margin: 'auto',
-                        width: '100%'
-                    }}
-                />
-                <img
-                    src="https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-                    style={{
-                        display: 'block',
-                        height: '100%',
-                        margin: 'auto',
-                        width: '100%'
-                    }}
-                />
-            </Carousel>
-            <div class="containerBox">
-                <div class="text-box">
-                    <h4>Food descrition</h4>
-                </div>
-                <img class="img-responsive" src={ pic } />
+        <div class="container">
+            <div class="carousel-item">
+                <Carousel
+                    additionalTransfrom={0}
+                    arrows
+                    autoPlaySpeed={3000}
+                    centerMode={false}
+                    className=""
+                    containerClass=""
+                    dotListClass=""
+                    draggable
+                    focusOnSelect={false}
+                    infinite
+                    itemClass=""
+                    keyBoardControl
+                    minimumTouchDrag={80}
+                    pauseOnHover
+                    renderArrowsWhenDisabled={false}
+                    renderButtonGroupOutside={false}
+                    renderDotsOutside={false}
+                    responsive={responsive}
+                    rewind={false}
+                    rewindWithAnimation={false}
+                    rtl={false}
+                    shouldResetAutoplay
+                    showDots
+                    sliderClass=""
+                    slidesToSlide={1}
+                    swipeable
+                    display="grid"
+                >
+
+                    <img
+                        src="https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+                        style={{
+                            display: 'block',
+                            height: '100%',
+                            margin: 'auto',
+                            width: '100%'
+                        }}
+                    />
+                    <img
+                        src="https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+                        style={{
+                            display: 'block',
+                            height: '100%',
+                            margin: 'auto',
+                            width: '100%'
+                        }}
+                    />
+                </Carousel>
             </div>
 
-            <div class="containerBox">
-                <div class="text-box">
-                    <h4>Your Text is responsive and centered</h4>
-                </div>
-                <img class="img-responsive" src={pic} />
+            <div class="item">
+                <img class="test" src={pic}></img>
             </div>
 
-            <div class="containerBox">
-                <div class="text-box">
-                    <h4>Your Text is responsive and centered</h4>
-                </div>
-                <img class="img-responsive" src={ pic } />
+            <div class="item">
+                <img class= "test" src={pic2}></img>
             </div>
 
-            <div class="thirdRowContainerBox">
-                <img class="" src={pic} />
+            <div class="item">
+                <img class="test" src={pic2}></img>
             </div>
-            <div class="thirdRowContainerBox2">
-                <div class="thirdRowWelcome">WELCOME</div>
-                <div class="thirdRowText">Tacos El Gordo has a passion for delivering great-tasting Mexican Tacos. We have been in the food service industry for over 40 years, opening our first restaurant in 1972.</div>
-                <button onClick={() => alert("Hello!")} class="button"> <span>More About Us</span></button>
-            </div>
+            
+            
+            <div class="break"></div>
+            <div class="item">third 3</div>
 
-            </div>
+        </div>
 
     );
-    
+
 }
 
 
